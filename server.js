085c57a777c1ab6a4a27121bf8bddc719c39a6c3
ktmsid390 <31263363+ktmsid390@users.noreply.github.com>
2017-09-03 11:27:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('http://ktmsid390.imad.hasura-app.io/1', function (req, res) {
+app.get('/ui/1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.html'));
 });
 
